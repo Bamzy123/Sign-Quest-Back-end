f9a98bb5a60f6c872c0aa26a5c4a4b87148dba05
Bamzy123 <stephenomotos@gmail.com>
2025-08-15 14:38:33 +0100

    update on database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
+
+  const mongoURL = process.env.MONGO_URI;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('MongoDB Connected')
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+        .then(() => console.log('MongoDB connected...'))
   } catch (err) {
     console.log('MongoDB connection error: ', err)
       process.exit(1);
